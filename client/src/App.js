d1b49bf4644391c12442a7ab8e143d46fc8a671a
philipk23 <78852273+philipk23@users.noreply.github.com>
2022-06-13 11:31:56 +0300

    Adding CSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import logo from './logo.svg';
-import './App.css';
+import style from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className={style.app}>
       <h1>Let's Begin</h1>
     </div>
   );
