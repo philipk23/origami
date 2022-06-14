483b8594a72402d43030a0ce977884b47793ea59
philipk23 <78852273+philipk23@users.noreply.github.com>
2022-06-14 12:02:50 +0300

    Adding header

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import style from './App.module.css';
+import Header from './components/Header/Header';
 
 //      ul>li.listItem*11>a[href=#]{Going to $ }
 
@@ -7,22 +8,7 @@ import style from './App.module.css';
 function App() {
   return (
     <div className={style.app}>
-      <nav className={style.navigation}>
-        <ul>
-          <li className="listItem"><img src="white-origami-bird.png" alt="White origami" /></li>
-          <li className="listItem"><a href="#">Going to 1</a></li>
-          <li className="listItem"><a href="#">Going to 2</a></li>
-          <li className="listItem"><a href="#">Going to 3</a></li>
-          <li className="listItem"><a href="#">Going to 4</a></li>
-          <li className="listItem"><a href="#">Going to 5</a></li>
-          <li className="listItem"><a href="#">Going to 6</a></li>
-          <li className="listItem"><a href="#">Going to 7</a></li>
-          <li className="listItem"><a href="#">Going to 8</a></li>
-          <li className="listItem"><a href="#">Going to 9</a></li>
-          <li className="listItem"><a href="#">Going to 10</a></li>
-          <li className="listItem"><a href="#">Going to 11</a></li>
-        </ul>
-      </nav>
+      <Header/>
     </div>
   );
 }
